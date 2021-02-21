d445d3e1f613a326ab586234c080cfef5ab2d3de
shashank <shashank.w1998@gmail.com>
2021-02-21 15:28:16 +0530

    case6 added

--- usec1.sh
+++ usec1.sh
@@ -14,3 +14,23 @@ echo $val3
 val4=$(($(($a % $a)) + $b))
 echo $val4
 
+#!/bin/bash
+read -p "Enter First Number." a
+read -p "Enter Second Number." b
+read -p  "Enter Third Number." c
+val=$(($(($a + $b)) * $c))
+val2=$(($(($a * $b)) + $c))
+val3=$(($(($c + $a)) / $b))
+val4=$(($(($a % $b)) + $c))
+echo "result for key1=" $val
+echo "result for key2=" $val2
+echo "result for key3=" $val3
+echo "result for key4=" $val4
+
+declare -A record
+record[key1]=$val
+record[key2]=$val2
+record[key3]=$val3
+record[key4]=$val4
+echo ${record[@]}
+
